41e956bdc5b2257643ab57f2d1380b064bee84cd
Daniil Zagorodnij <zagorodnijdaniil@yandex.ru>
2019-09-08 16:43:24 +0400

    second commit br.develop

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 
 const reg = /([А-ЯЁ][а-яё]+|[А-ЯЁ][а-яё]+-[А-ЯЁ][а-яё]+)/g;
-const regMaie = /(\w+|\-\w+)@[a-z]{1,}\.[a-z]{1,}/g; 
-const regphone = /\+?\s?\d\s?\D?\(?\s?\d{3}\s?\)?\s?\D?\s?\d{3}\s?\D?\s?\d\d\s?\D?\s?\d\d/g;   
-const regUrl = /http[s]?(:)\/\/\S{1,}/g; 
+const regMaie = /(\w+|\-\w+)@[a-z]{1,}\.[a-z]{1,}/g;
+const regphone = /\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*\d\D*/g;  
+const regUrl = /http[s]?(:)\/\/\S{1,}/g;
 
 const button = document.querySelector('.form__button');
 const form = document.forms.register;
@@ -11,11 +11,26 @@ const nameFormInput = form.elements.name1;
 const emailFormInput = form.elements.name2;
 const phoneFormInput = form.elements.name3;
 const urlFormInput = form.elements.name4;
+let num = null;
+let resul = null;
+let regg = /\d/g;
 
-button.addEventListener('click', ()=>{
-    if (urlFormInput.value.match(regUrl)){
-        console.log(urlFormInput.value.match(regUrl));
+function arrMap(item) {
+    item.map((item) => {
+        //    console.log(item.match(regg));
+        resul = item.match(regg);
+    });
+}
+
+button.addEventListener('click', () => {
+    if (phoneFormInput.value.match(regphone)) {
+        // console.log(phoneFormInput.value);
+        console.log(phoneFormInput.value.match(regphone));
+        num = phoneFormInput.value.match(regphone);
+        arrMap(num);
+        console.log(resul);
     } else {
         console.log('неа')
     }
 });
+
